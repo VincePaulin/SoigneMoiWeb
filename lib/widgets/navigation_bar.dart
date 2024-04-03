66e980195f5f793b16900c72cd4a8a516df0e6a7
Vincent Paulin <ecniv81@gmail.com>
2024-04-03 13:34:30 +0200

    feat: nav title logo and design

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -11,9 +11,33 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 300.0,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
       color: const Color(0xFF0072BB),
-      child: ListView(
+      child: Column(
         children: <Widget>[
+          Container(
+            width: 150,
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Column(
+              children: [
+                Image.asset('assets/img/logo.png'),
+                Text(
+                  "Soigne Moi",
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black.withOpacity(0.3),
+                        offset: const Offset(1, 1),
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
           _buildNavItem('Home', Icons.home, 0),
           _buildNavItem('Profile', Icons.person, 1),
           _buildNavItem('Settings', Icons.settings, 2),
@@ -25,7 +49,10 @@ class NavBar extends StatelessWidget {
   Widget _buildNavItem(String title, IconData iconData, int index) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-      color: selectedIndex == index ? Colors.lightBlueAccent : null,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: selectedIndex == index ? Colors.white : null,
+      ),
       child: ListTile(
         leading: Icon(
           iconData,
@@ -34,7 +61,8 @@ class NavBar extends StatelessWidget {
         title: Text(
           title,
           style: TextStyle(
-            color: selectedIndex == index ? Colors.black : Colors.lightBlueAccent,
+            color:
+                selectedIndex == index ? Colors.black : Colors.lightBlueAccent,
           ),
         ),
         onTap: () => onItemTapped(index),
