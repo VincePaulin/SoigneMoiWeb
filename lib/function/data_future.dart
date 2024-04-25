bf091c0a884dc4636ebdab9e995bf8b24efc5a0f
Vincent Paulin <ecniv81@gmail.com>
2024-04-25 09:29:38 +0200

    feat: future fetchStays

--- lib/function/data_future.dart
+++ lib/function/data_future.dart
@@ -1,7 +1,7 @@
-import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:soigne_moi_web/config/app_config.dart';
+import 'package:soigne_moi_web/model/stay.dart';
 import 'package:soigne_moi_web/model/user.dart';
 
 Future<User> fetchUser() async {
@@ -27,5 +27,33 @@ Future<User> fetchUser() async {
     }
   } catch (e) {
     throw Exception('Failed to fetch user: $e');
+  }
+}
+
+Future<List<Stay>> fetchStays() async {
+  final storage = FlutterSecureStorage();
+  final token = await storage.read(key: 'access_token');
+
+  final dio = Dio();
+  dio.options.baseUrl = AppConfig.baseUrl;
+
+  try {
+    final response = await dio.get(
+      '/user/stays',
+      options: Options(headers: {
+        'Authorization': 'Bearer $token',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> staysJson = response.data['stays'];
+      final List<Stay> stays =
+          staysJson.map((json) => Stay.fromJson(json)).toList();
+      return stays;
+    } else {
+      throw Exception('Failed to fetch stays');
+    }
+  } catch (e) {
+    throw Exception('Failed to fetch stays: $e');
   }
 }
--- lib/model/stay.dart
+++ lib/model/stay.dart
@@ -14,4 +14,14 @@ class Stay {
       required this.startDate,
       required this.endDate,
       this.precision});
+
+  factory Stay.fromJson(Map<String, dynamic> json) {
+    return Stay(
+      motif: json['motif'] ?? '',
+      type: json['type'] ?? '',
+      startDate: DateTime.parse(json['startDate']),
+      endDate: DateTime.parse(json['endDate']),
+      precision: json['precision'],
+    );
+  }
 }
