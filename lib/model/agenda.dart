e6a3e7a8ab77fd678d3d29c76cf2142caa7cc6d5
Vincent Paulin <ecniv81@gmail.com>
2024-05-11 15:24:35 +0200

    feat: model agenda

--- lib/model/agenda.dart
+++ lib/model/agenda.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soigne_moi_web/model/doctor.dart';
 
 // Template for showing an appointment
 class Appointment {
@@ -22,12 +23,22 @@ class Appointment {
 // Model to represent a doctor's schedule
 class Agenda {
   final String id;
-  final String doctorMatricule;
+  final Doctor doctor;
   final List<Appointment> appointments;
 
   Agenda({
     required this.id,
-    required this.doctorMatricule,
+    required this.doctor,
     required this.appointments,
   });
+
+  // Function to convert JSON to Agenda object
+  factory Agenda.fromJson(Map<String, dynamic> json) {
+    return Agenda(
+      id: json['id'].toString(),
+      doctor: Doctor.fromJson(json['doctor']),
+      appointments: [],
+      //appointments: List<Appointment>.from(json['appointments'].map((x) => Appointment.fromJson(x))),
+    );
+  }
 }
