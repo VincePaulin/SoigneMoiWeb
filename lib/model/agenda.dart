6b2c578fa44bc829dcf6912a0ab6994f3330548e
Vincent Paulin <ecniv81@gmail.com>
2024-05-12 19:23:26 +0200

    feat: modify models

--- lib/model/agenda.dart
+++ lib/model/agenda.dart
@@ -1,26 +1,45 @@
 import 'package:soigne_moi_web/model/doctor.dart';
 
 // Template for showing an appointment
+import 'dart:convert';
+
 class Appointment {
-  final String id;
-  final String title;
+  final String? id;
   final DateTime startDate;
   final DateTime endDate;
-  final String description;
   final String patientId;
   final String doctorMatricule;
   final String stayId;
 
   Appointment({
-    required this.id,
-    required this.title,
+    this.id,
     required this.startDate,
     required this.endDate,
-    required this.description,
     required this.patientId,
     required this.doctorMatricule,
     required this.stayId,
   });
+
+  factory Appointment.fromJson(Map<String, dynamic> json) {
+    return Appointment(
+      id: json['id'],
+      startDate: DateTime.parse(json['start_date']),
+      endDate: DateTime.parse(json['end_date']),
+      patientId: json['patient_id'],
+      doctorMatricule: json['doctor_matricule'],
+      stayId: json['stay_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'start_date': startDate.toIso8601String(),
+      'end_date': endDate.toIso8601String(),
+      'patient_id': patientId,
+      'doctor_matricule': doctorMatricule,
+      'stay_id': stayId,
+    };
+  }
 }
 
 // Model to represent a doctor's schedule
