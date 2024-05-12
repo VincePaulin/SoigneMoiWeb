b17c35f40b5f57058d8a14e079bc50e5dc89be2b
Vincent Paulin <ecniv81@gmail.com>
2024-05-12 13:45:45 +0200

    feat: calendar modif

--- lib/page/admin/agendas/calendar/calendar_appointments.dart
+++ lib/page/admin/agendas/calendar/calendar_appointments.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 import 'package:soigne_moi_web/model/agenda.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarDoctorWidget extends StatefulWidget {
   final Agenda agenda;
 
-  const CalendarDoctorWidget({super.key, required this.agenda});
+  const CalendarDoctorWidget({Key? key, required this.agenda})
+      : super(key: key);
 
   @override
   State<CalendarDoctorWidget> createState() => _CalendarDoctorWidgetState();
@@ -26,86 +27,139 @@ class _CalendarDoctorWidgetState extends State<CalendarDoctorWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      firstDay: DateTime.utc(2010, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: _focusedDay,
-      calendarFormat: _calendarFormat,
-      onFormatChanged: (format) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildFormatButton(CalendarFormat.month),
+            _buildFormatButton(CalendarFormat.week),
+            _buildFormatButton(CalendarFormat.twoWeeks),
+          ],
+        ),
+        TableCalendar(
+          headerStyle: const HeaderStyle(formatButtonVisible: false),
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: _focusedDay,
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          },
+          onPageChanged: (focusedDay) {
+            setState(() {
+              _focusedDay = focusedDay;
+            });
+          },
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+            });
+          },
+          headerVisible: true,
+          calendarBuilders: CalendarBuilders(
+            defaultBuilder: (context, date, events) {
+              final isToday = isSameDay(date, DateTime.now());
+              final isPastDay = date.isBefore(DateTime.now());
+              final isFutureDay = date.isAfter(DateTime.now());
+
+              BoxDecoration? decoration;
+
+              if (isToday) {
+                decoration = BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                );
+              } else if (isPastDay) {
+                decoration = BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey.withOpacity(0.3),
+                );
+              } else if (isFutureDay) {
+                decoration = BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                );
+              }
+
+              return Container(
+                margin: const EdgeInsets.all(4.0),
+                decoration: decoration,
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: TextStyle(
+                      color: isPastDay ? Colors.grey : Colors.black,
+                    ),
+                  ),
+                ),
+              );
+            },
+            markerBuilder: (context, date, events) {
+              final appointmentsForCurrentDate = widget.agenda.appointments
+                  .where(
+                      (appointment) => isSameDay(appointment.startDate, date))
+                  .toList();
+
+              if (appointmentsForCurrentDate.isNotEmpty) {
+                return CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 10,
+                  child: Text(
+                    '${appointmentsForCurrentDate.length}',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  ),
+                );
+              } else {
+                return null;
+              }
+            },
+          ),
+          daysOfWeekVisible: true,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFormatButton(CalendarFormat format) {
+    return TextButton(
+      onPressed: () {
         setState(() {
           _calendarFormat = format;
         });
       },
-      onPageChanged: (focusedDay) {
-        setState(() {
-          _focusedDay = focusedDay;
-        });
-      },
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-      onDaySelected: null, // Disable day selection
-      headerVisible: false, // Hide the entire header
-      calendarBuilders: CalendarBuilders(
-        defaultBuilder: (context, date, events) {
-          final isToday = isSameDay(date, DateTime.now());
-          final isPastDay = date.isBefore(DateTime.now());
-          final isFutureDay = date.isAfter(DateTime.now());
-
-          BoxDecoration decoration;
-
-          if (isToday) {
-            decoration = BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
-            );
-          } else if (isPastDay) {
-            decoration = BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.grey.withOpacity(0.3),
-            );
-          } else if (isFutureDay) {
-            decoration = BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-            );
-          }
-
-          return Container(
-            margin: const EdgeInsets.all(4.0),
-            //decoration: decoration,
-            child: Center(
-              child: Text(
-                '${date.day}',
-                style: TextStyle(
-                  color: isPastDay ? Colors.grey : Colors.black,
-                ),
-              ),
-            ),
-          );
-        },
-        markerBuilder: (context, date, events) {
-          final appointmentsForCurrentDate = widget.agenda.appointments
-              .where((appointment) => isSameDay(appointment.startDate, date))
-              .toList();
-
-          if (appointmentsForCurrentDate.isNotEmpty) {
-            return CircleAvatar(
-              backgroundColor: Colors.red,
-              radius: 10,
-              child: Text(
-                '${appointmentsForCurrentDate.length}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-                ),
-              ),
-            );
-          } else {
-            return null;
-          }
-        },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith((states) {
+          return _calendarFormat == format ? Colors.blue : null;
+        }),
+      ),
+      child: Text(
+        _getFormatText(format),
+        style: TextStyle(
+          color: _calendarFormat == format ? Colors.white : null,
+        ),
       ),
     );
+  }
+
+  String _getFormatText(CalendarFormat format) {
+    switch (format) {
+      case CalendarFormat.month:
+        return 'Month';
+      case CalendarFormat.week:
+        return 'Week';
+      case CalendarFormat.twoWeeks:
+        return '2 Weeks';
+      default:
+        return '';
+    }
   }
 }
